0fe1e5d8a4eb3c48ae0466cac0faf7b49d0d351c
Farm <67070223@kmitl.ac.th>
2026-02-11 18:49:29 +0700

    42

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print("Give me a word: ", end="")
+word = input()
+
+word = word.upper()
+print(word)
---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+fnum = int(input("Give me the first number: "))
+snum = int(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(str(fnum) + " + " + str(snum) + " = " + str(fnum+snum))
+print(str(fnum) + " - " + str(snum) + " = " + str(fnum-snum))
+print(str(fnum) + " / " + str(snum) + " = " + str(int(fnum/snum)))
+print(str(fnum) + " * " + str(snum) + " = " + str(int(fnum*snum)))
