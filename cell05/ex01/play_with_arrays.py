b99c3c491080ec145b6792ebfd1aae2699a2cf16
Farm <67070223@kmitl.ac.th>
2026-02-12 12:58:57 +0700

    42

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+arr =  [2, 8, 9, 48, 8, 22, -12, 2]
+keep = []
+
+for i in arr:
+    i += 2
+    keep.append(i)
+
+print("Original array: ", arr)
+print("New array: ", keep)
